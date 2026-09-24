f42b6140017d9beef68d4542143d5b2d32e2d6c2
tramcam2706-afk <tramcam2706@gmail.com>
2026-09-24 13:59:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CSV_FILE = "history.csv"
 # Thực đơn cố định của nhà hàng Mr. Bình
 menu = {
    "Đồ ăn": {
-       "Pizza Hải Sản": 150000,
+       "Pizza Hải Sản": 150000,"Pizza người": 500000,
        "Mì Ý Bò Bằm": 95000,
        "Burger Gà": 65000,
        "Salad Trộn": 50000,
@@ -23,6 +23,7 @@ menu = {
    },
    "Thức uống": {
        "Coca Cola": 20000,
+       "Trà tắc" :40000,
        "Trà Đào Cam Sả": 35000,
        "Cà Phê Sữa": 25000,
        "Nước Suối": 10000,
